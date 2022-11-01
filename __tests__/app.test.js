cbb8841c982c9e5e7486b6cb15540ea7065a32d8
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-01 10:42:52 -0700

    got tests to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const { zodiac } = require('../lib/zodiac-data');
 
 describe('zodiac route', () => {
   it('/ should return a list of all zodiacs', async () => {
-    const resp = await request(app).get('/');
+    const resp = await request(app).get('/zodiac');
     const expected = zodiac.map((zodiacs) => {
       return { id: zodiacs.id, name: zodiacs.name, dates: zodiacs.dates, symbol: zodiacs.symbol, horoscope: zodiacs.horoscope };
     });
@@ -26,13 +26,13 @@ describe('zodiac route', () => {
   });
 
 
-  it('/horoscopes/:sign'), async () => {
-    const resp = await request(app).get('/horoscopes/aquarius');
-    const symbol = {
-      horoscope: 'Will you go out with me? Be careful if a person says yes when you ask that question today, Aquarius. You could take the nature of this situation to the extreme. Saying yes doesnt mean youre suddenly in charge of his or her life. Nor are you responsible for anything that person does or how they feel. If youre still asking the question without getting any positive responses, dont worry. Keep trying.'
-    };
+  it('/zodiac/horoscope/:sign', async () => {
+    const resp = await request(app).get('/zodiac/horoscope/cancer');
+    const symbol =
+      'There is irony in todays situation, Cancer.The only real remedy for situations like this is to accept them and joke about it.If you try to take yourself too seriously, especially when it comes to art or romance, you will inevitably fail.Your heart is sensual and your emotions profound.Bring yourself into balance by laughing instead of crying about the events of the day.'
+    ;
     expect(resp.body).toEqual(symbol);
-  };
+  });
 
 
 });
